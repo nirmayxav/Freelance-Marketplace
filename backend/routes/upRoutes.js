fd9a654058b23f9799dd5906d0f484a9f414a25f
nirmayxav <nirmay.ab@gmail.com>
2025-04-04 13:40:03 +0530

    application between users gets created

--- backend/routes/upRoutes.js
+++ backend/routes/upRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const protect = require('../middleware/auth'); // Your auth middleware
+const protect = require('../middlewares/authMiddleware');
 
 // PUT /api/rewards/update-coins
 router.put('/update-coins', protect, async (req, res) => {
